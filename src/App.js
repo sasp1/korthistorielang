05a50e93998a9b9a8d93d6a143c4e119d90a131d
Sebastian Arcos Specht <s164394@student.dtu.dk>
2021-03-05 14:57:53 +0100

    committing changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Route, Switch, Redirect} from "react-router-dom"
 import Login from "./components/login";
 import Home from "./components/home";
 
+class App extends Component {
 
 
-function App() {
-  return (
-    <div className="App container h-100">
-        <Switch>
-          <Route path={ "/login"} render={() => <Login />}/>
-          <Route path={ "/"} exact render={() => <Home loggedIn={false}/>}/>
-          <Redirect to={ "/not-found"}/>
-        </Switch>
-    </div>
-  );
+    onLogin = () => {
+        console.log("HEEEEJ")
+        this.props.history.replace("/");
+    }
+
+    render(){
+        return (
+            <div className="App container h-100">
+                <Switch>
+                    <Route path={"/login"} render={() => <Login onLogin={this.onLogin}/>}/>
+                    <Route path={"/"} exact render={() => <Home loggedIn={false}/>}/>
+                    <Redirect to={"/not-found"}/>
+                </Switch>
+            </div>
+        );
+    }
+
 }
-
 export default App;
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,20 +1,55 @@
 import React, {Component} from "react";
 import logo from "./logo.png";
+import http from "../services/httpService";
+import PropTypes from 'prop-types';
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            password: "",
+            error: undefined
+        };
+    }
+
+    handleChange = ({currentTarget: input}) => {
+        this.setState({password: input.value, error: ""});
+    };
+
     render() {
+        const {error, password} = this.state;
         return (
             <div className="container h-75 justify-content-center align-items-center d-flex flex-column">
                 <img src={logo} alt="Logo"/>
-                <form className="mt-n5">
+                <form className="mt-n5" onSubmit={this.handleSubmit}>
                     <div className="form-group ">
-                        <input type="email" className="form-control" id="inputEmail4" placeholder="Password"/>
+                        <input type="password" className="form-control" placeholder="Password"
+                               onChange={this.handleChange} value={password}/>
                     </div>
+                    {error && <div className="alert alert-danger mt-3">{error}</div>}
                     <button id="signin" className="btn btn-primary btn-block" type="submit">Sign in</button>
                 </form>
             </div>
         );
     }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const {password} = this.state;
+        try {
+            const res = await http.post("/todos/", {});
+            console.log("data", res.data);
+        } catch (e) {
+            console.log("error data", e.response.data);
+        }
+        this.props.onLogin();
+    }
 }
+
+Login.propTypes = {
+    onLogin: PropTypes.func.isRequired
+};
 
 export default Login;
